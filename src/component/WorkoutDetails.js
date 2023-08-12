f558ac0f857adc2957bf58d3e3c1b463bac075b9
AJay6600 <ajaymore0634@gmail.com>
2023-08-12 18:56:56 +0530

    Final Commit

--- src/component/WorkoutDetails.js
+++ src/component/WorkoutDetails.js
@@ -5,7 +5,7 @@ import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 const WorkoutDetails = ({workout}) => {
     const {dispatch} = useWorkoutsContext()
     const handleClick = async () => {
-        const response = await fetch('https://workoutbuddy-yeon.onrender.com/api/workout'+workout._id,{
+        const response = await fetch('https://workoutbuddy-yeon.onrender.com/api/workout/'+workout._id,{
             method:'DELETE'
         })
         const json = await response.json()
@@ -21,10 +21,10 @@ const WorkoutDetails = ({workout}) => {
       <p><strong>Load (kg): </strong> {workout.load}</p>
       <p><strong>Reps : </strong> {workout.reps}</p>
       <p>{formatDistanceToNow(new Date(workout.createdAt),{addSuffix:true})}</p>
-      <div className="icons">
+      
         <span className='material-symbols-outlined' onClick={handleClick}>delete</span>
      
-      </div>
+      
     </div>
   )
 }
